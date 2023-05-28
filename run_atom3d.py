00e408b138425ebfb431cb29c50cb29ef03125ee
erencantatar <erencantatar@gmail.com>
2023-05-28 12:04:44 +0200

    Update run_atom3d.py
    Updated with Bert intergration and WANDB to log models and loss

--- run_atom3d.py
+++ run_atom3d.py
@@ -2,6 +2,12 @@ import argparse
 import warnings
 import random
 
+
+warnings.filterwarnings('ignore')
+"""
+BiopythonDeprecationWarning: 'three_to_one' will be deprecated in a future release of Biopython in favor of 'Bio.PDB.Polypeptide.protein_letters_3to1'.
+"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument('task', metavar='TASK', choices=[
         'PSR', 'RSR', 'PPI', 'RES', 'MSP', 'SMP', 'LBA', 'LEP'
@@ -27,16 +33,18 @@ parser.add_argument('--lr', metavar='RATE', default=1e-4, type=float,
                     help='learning rate')
 parser.add_argument('--load', metavar='PATH', default=None, 
                     help='initialize first 2 GNN layers with pretrained weights')
+parser.add_argument('--load_fullModel', metavar='PATH', default=None, 
+                    help='load full model')
 parser.add_argument('--seed', metavar='N', type=int, required=True,
                     help='random seed')
 parser.add_argument('--transformer', action='store_true', default=False)
-parser.add_argument('--protein_bert', action='store_true', default=False)
-
+parser.add_argument('--bert_emb', action='store_true', default=False)
+parser.add_argument('--bert_prediction', action='store_true', default=False)
+parser.add_argument('--use_wandb', action='store_true', default=False)
 args = parser.parse_args()
 
 import gvp
 from atom3d.datasets import LMDBDataset
-from transformers import T5Tokenizer, T5EncoderModel
 import torch_geometric
 from functools import partial
 import gvp.atom3d
@@ -47,54 +55,70 @@ from atom3d.util import metrics
 import sklearn.metrics as sk_metrics
 from collections import defaultdict
 import scipy.stats as stats
-
+import wandb
 print = partial(print, flush=True)
 
+print("--------------------------")
 models_dir = 'models'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Device: ", device)
-
 model_id = float(time.time())
 
 import os
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
-    
 warnings.filterwarnings("ignore", category=UserWarning)
-    
 def main():
+
+    wandb.init(project="gvp", 
+                mode="online" if args.use_wandb else "offline",
+                config={         
+                "Lisa": True,
+                "learning_rate": args.lr,
+                "epochs": args.epochs,
+                "model_id": model_id,
+                "batch_size": args.batch,
+                "seed": args.seed,
+                "transformer": args.transformer,
+                "bert_emb": args.bert_emb,
+                "bert_prediction":args.bert_prediction,
+                "task": args.task,
+                "device": device,
+                "remark": "transformer + bert_emb + bert_prediction MLP",
+            }
+            )
+    wandb.config.update(args)
+
     datasets = get_datasets(args.task, args.lba_split)
-    dataloader = partial(torch_geometric.data.DataLoader, 
+    dataloader = partial(torch_geometric.loader.DataLoader, 
                     num_workers=args.num_workers, batch_size=args.batch)
     if args.task not in ['PPI', 'RES']:
         dataloader = partial(dataloader, shuffle=True)
-        
     trainset, valset, testset = map(dataloader, datasets)    
     model = get_model(args.task).to(device)
-    
-    # Intialize protein_bert model
-    bert_model = None
-    bert_tokenizer = None
-    if args.protein_bert:
-        transformer_link = "Rostlab/prot_t5_xl_half_uniref50-enc"
-        print("Loading: {}".format(transformer_link))
-        bert_model = T5EncoderModel.from_pretrained(transformer_link)
-        bert_model.full() if device=='cpu' else bert_model.half() # only cast to full-precision if no GPU is available
-        bert_model = bert_model.to(device)
-        bert_model = bert_model.eval()
-        bert_tokenizer = T5Tokenizer.from_pretrained(transformer_link, do_lower_case=False)
 
+    # Magic
+    wandb.watch(model, log_freq=100)
+
+    print("-----------Info-----------\n\n")
+    if args.bert_emb:
+        print("doing protein bert")
     if args.test:
         print("--------testing--------")
         test(model, testset)
-
     else:
-        seed_torch(args.seed)
         if args.load:
             print("--------loading model--------")
             load(model, args.load)
+        if args.load_fullModel:
+            print("--------loading full model--------")
+            model = get_model(args.task).to(device)
+            model.load_state_dict(torch.load(args.load_fullModel))
         print("--------training model--------")
         train(model, trainset, valset)
+
+    # [optional] finish the wandb run, necessary in notebooks
+    wandb.finish()
         
 def test(model, testset):
     model.load_state_dict(torch.load(args.test))
@@ -126,10 +150,13 @@ def train(model, trainset, valset):
     best_path, best_val = None, np.inf
     
     # Make model directory
-    if args.transformer:
+    if args.transformer and not args.bert_emb:
+        # Transformer normal has 8 heads
         root = f"{models_dir}/{args.task}/Transformer/{args.seed}/"
-    elif args.protein_bert:
-        root = f"{models_dir}/{args.task}/ProteinBert/{args.seed}/"
+    elif args.transformer and args.bert_emb and not args.bert_prediction:
+        root = f"{models_dir}/{args.task}/Transformer_bertEmb/{args.seed}/"
+    elif args.transformer and args.bert_emb and args.bert_prediction:
+        root = f"{models_dir}/{args.task}/GVP_Trans_bertPredic/{args.seed}/"
     else:
         root = f"{models_dir}/{args.task}/GVP/{args.seed}/"
     
@@ -137,39 +164,42 @@ def train(model, trainset, valset):
         os.makedirs(root)
     
     for epoch in range(args.epochs):    
+        print(f"-----epoch {epoch}-----")
         # Model save path
         if args.transformer:
             path = os.path.join(root, f"{args.task}_{model_id}_{epoch}_TF.pt")
-        elif args.protein_bert:
+        elif args.bert_emb:
             path = os.path.join(root, f"{args.task}_{model_id}_{epoch}_PB.pt")
         else:
             path = os.path.join(root, f"{args.task}_{model_id}_{epoch}_GVP.pt")
         
         model.train()
-        loss = loop(trainset, model, optimizer=optimizer, max_time=args.train_time)
-        
+        loss = loop(trainset, model, dataset_type="train", optimizer=optimizer, max_time=args.train_time)
         torch.save(model.state_dict(), path)
         print(f'\nEPOCH {epoch} TRAIN loss: {loss:.8f}')
         model.eval()
         with torch.no_grad():
-            loss = loop(valset, model, max_time=args.val_time)
+            loss = loop(valset, model, dataset_type="validation", max_time=args.val_time)
         print(f'\nEPOCH {epoch} VAL loss: {loss:.8f}')
         if loss < best_val:
             best_path, best_val = path, loss
         print(f'BEST {best_path} VAL loss: {best_val:.8f}')
+
+        wandb.log({"loss": loss})
     
-def loop(dataset, model, optimizer=None, max_time=None):
+def loop(dataset, model, dataset_type="train", optimizer=None, max_time=None):
     start = time.time()
     
     loss_fn = get_loss(args.task)
     t = tqdm.tqdm(dataset)
     total_loss, total_count = 0, 0
     
-    for batch in t:
+    log_interval = 10
+    for batch_idx, (batch) in enumerate(t):
+
         if max_time and (time.time() - start) > 60*max_time: break
         if optimizer: optimizer.zero_grad()
         try:
-            sequence = get_sequence(batch)
             out = forward(model, batch, device)
         except RuntimeError as e:
             if "CUDA out of memory" not in str(e): raise(e)
@@ -178,14 +208,13 @@ def loop(dataset, model, optimizer=None, max_time=None):
             continue
             
         label = get_label(batch, args.task, args.smp_idx)
-        # loss_value = loss_fn(out, label)
-        loss_value = loss_fn(out, label.float())
-
+        loss_value = loss_fn(out, label)
         total_loss += float(loss_value)
         total_count += 1
         
         if optimizer:
             try:
+                wandb.log({f"{dataset_type}_loss": loss_value})
                 loss_value.backward()
                 optimizer.step()
             except RuntimeError as e:
@@ -193,8 +222,12 @@ def loop(dataset, model, optimizer=None, max_time=None):
                 torch.cuda.empty_cache()
                 print('Skipped batch due to OOM', flush=True)
                 continue
-            
-        t.set_description(f"{total_loss/total_count:.8f}")
+      
+        if batch_idx % log_interval == 0:
+            wandb.log({f"{dataset_type}_batchidx_loss": loss_value})
+
+           
+        t.set_description(f"Avg loss: {total_loss/total_count:.8f} \t Total: {total_loss}, {total_count:.8f}")
         
     return total_loss / total_count
 
@@ -209,9 +242,6 @@ def load(model, path):
             model.state_dict()[name].copy_(p)
         
 #######################################################################
-## TODO: Implement get_sequence
-def get_sequence(batch):
-    pass
 
 def get_label(batch, task, smp_idx=None):
     if type(batch) in [list, tuple]: batch = batch[0]
@@ -263,23 +293,26 @@ def forward(model, batch, device):
 def get_datasets(task, lba_split=30):
     data_path = {
         'RES' : 'data/atom3d-data/RES/raw/RES/data/',
-        'PPI' : 'data/atom3d-data/PPI/splits/DIPS-split/data/',
-        'RSR' : 'data/atom3d-data/RSR/splits/candidates-split-by-time/data/',
-        'PSR' : 'data/atom3d-data/PSR/splits/split-by-year/data/',
+                 
+        'PPI' : 'atom3d-data/PPI/splits/DIPS-split/data/',
+        'RSR' : 'atom3d-data/RSR/splits/candidates-split-by-time/data/',
+        'PSR' : 'atom3d-data/PSR/splits/split-by-year/data/',
         'MSP' : 'data/atom3d-data/MSP/splits/split-by-sequence-identity-30/data/',
-        'LEP' : 'data/atom3d-data/LEP/splits/split-by-protein/data/',
-        'LBA' : f'data/atom3d-data/LBA/splits/split-by-sequence-identity-{lba_split}/data/',
+        'LEP' : 'atom3d-data/LEP/splits/split-by-protein/data/',
+        'LBA' : f'atom3d-data/LBA/splits/split-by-sequence-identity-{lba_split}/data/',
         'SMP' : 'data/atom3d-data/SMP/splits/random/data/'
     }[task]
         
     if task == 'RES':
-        
-        # split_path = 'data/atom3d-data/RES/splits/split-by-cath-topology/indices/'     
-        split_path = 'data/atom3d-data/RES/raw/RES/data/indices/'  
-        
-        trainset = gvp.atom3d.RESDataset(data_path, split_path=split_path+'train_indices.txt')
-        valset = gvp.atom3d.RESDataset(data_path, split_path=split_path+'val_indices.txt')
-        testset = gvp.atom3d.RESDataset(data_path, split_path=split_path+'test_indices.txt')
+        # split_path = 'atom3d-data/RES/splits/split-by-cath-topology/indices/'
+        split_path = 'data/atom3d-data/RES/raw/RES/data/indices/'
+        # split_path = 'data/atom3d-data/RES/raw/RES/data/indices/'
+        # split_path = 'atom3d-data/MSP/splits/split-by-sequence-identity-30/indices/'
+
+        dataset = partial(gvp.atom3d.RESDataset, data_path)        
+        trainset = dataset(split_path=split_path+'train_indices.txt')
+        valset = dataset(split_path=split_path+'val_indices.txt')
+        testset = dataset(split_path=split_path+'test_indices.txt')
     
     elif task == 'PPI':
         trainset = gvp.atom3d.PPIDataset(data_path+'train')
@@ -307,10 +340,13 @@ def get_model(task):
         print("Using TransformerConv")
     else:
         print("Using GVPConv")
-    if args.protein_bert:
-        print("Using ProteinBert")
+    if args.bert_emb:
+        print("Using ProteinBert embedding ")
+    if args.bert_prediction:
+        print("Using ProteinBert prediction ")
     return {
-        'RES' : gvp.atom3d.RESModel(use_transformer=args.transformer, use_protein_bert=args.protein_bert),
+        'RES' : gvp.atom3d.RESModel(use_transformer=args.transformer, 
+                                    use_bert_embedding=args.bert_emb, use_bert_predict=args.bert_prediction),
         'PPI' : gvp.atom3d.PPIModel(use_transformer=args.transformer),
         'RSR' : gvp.atom3d.RSRModel(use_transformer=args.transformer),
         'PSR' : gvp.atom3d.PSRModel(use_transformer=args.transformer),
@@ -319,7 +355,7 @@ def get_model(task):
         'LBA' : gvp.atom3d.LBAModel(use_transformer=args.transformer),
         'SMP' : gvp.atom3d.SMPModel(use_transformer=args.transformer)
     }[task]
-    
+
 def seed_torch(seed):
     random.seed(seed)
     np.random.seed(seed)
